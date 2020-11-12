4f41e64bb68c00b620f20f8b5c1ff68213f3f289
ralcini <roberto.alcini@gmail.com>
2020-11-12 02:27:47 +0100

    Configurable value for time drift between client/server for authentication request issue1065 (#1070)
    * Issue 1065
    
    * feat: Allow to configure a custom value for time drift between client/server for authentication
    
    The use case is to support scenarios where it's not possible to enforce sync between client and server times.
    
    * enh: drift redefined with skew
    
    Co-authored-by: Francesco Marino <francesco.marino@cybaze.it>
    Change-Id: I0865c12ed85aa0cdb18e5f9671b448a15b015da3

--- src/iperf_client_api.c
+++ src/iperf_client_api.c
@@ -1,5 +1,5 @@
 /*
- * iperf, Copyright (c) 2014-2018, The Regents of the University of
+ * iperf, Copyright (c) 2014-2020, The Regents of the University of
  * California, through Lawrence Berkeley National Laboratory (subject
  * to receipt of any required approvals from the U.S. Dept. of
  * Energy).  All rights reserved.
@@ -485,7 +485,7 @@ iperf_run_client(struct iperf_test * test)
 
     /* Start the client and connect to the server */
     if (iperf_connect(test) < 0)
-        return -1;
+        goto cleanup_and_fail;
 
     /* Begin calculating CPU utilization */
     cpu_util(NULL);
@@ -496,15 +496,15 @@ iperf_run_client(struct iperf_test * test)
 	memcpy(&write_set, &test->write_set, sizeof(fd_set));
 	iperf_time_now(&now);
 	timeout = tmr_timeout(&now);
-	result = select(test->max_fd + 1, &read_set, test->state == TEST_RUNNING ? &write_set : NULL, NULL, timeout);
+	result = select(test->max_fd + 1, &read_set, &write_set, NULL, timeout);
 	if (result < 0 && errno != EINTR) {
   	    i_errno = IESELECT;
-	    return -1;
+	    goto cleanup_and_fail;
 	}
 	if (result > 0) {
 	    if (FD_ISSET(test->ctrl_sck, &read_set)) {
  	        if (iperf_handle_message_client(test) < 0) {
-		    return -1;
+		    goto cleanup_and_fail;
 		}
 		FD_CLR(test->ctrl_sck, &read_set);
 	    }
@@ -528,17 +528,17 @@ iperf_run_client(struct iperf_test * test)
 	    if (test->mode == BIDIRECTIONAL)
 	    {
                 if (iperf_send(test, &write_set) < 0)
-                    return -1;
+                    goto cleanup_and_fail;
                 if (iperf_recv(test, &read_set) < 0)
-                    return -1;
+                    goto cleanup_and_fail;
 	    } else if (test->mode == SENDER) {
                 // Regular mode. Client sends.
                 if (iperf_send(test, &write_set) < 0)
-                    return -1;
+                    goto cleanup_and_fail;
 	    } else {
                 // Reverse mode. Client receives.
                 if (iperf_recv(test, &read_set) < 0)
-                    return -1;
+                    goto cleanup_and_fail;
 	    }
 
 
@@ -546,11 +546,20 @@ iperf_run_client(struct iperf_test * test)
             iperf_time_now(&now);
             tmr_run(&now);
 
-	    /* Is the test done yet? */
+	    /*
+	     * Is the test done yet?  We have to be out of omitting
+	     * mode, and then we have to have fulfilled one of the
+	     * ending criteria, either by times, bytes, or blocks.
+	     * The bytes and blocks tests needs to handle both the
+	     * cases of the client being the sender and the client
+	     * being the receiver.
+	     */
 	    if ((!test->omitting) &&
 	        ((test->duration != 0 && test->done) ||
-	         (test->settings->bytes != 0 && test->bytes_sent >= test->settings->bytes) ||
-	         (test->settings->blocks != 0 && test->blocks_sent >= test->settings->blocks))) {
+	         (test->settings->bytes != 0 && (test->bytes_sent >= test->settings->bytes ||
+						 test->bytes_received >= test->settings->bytes)) ||
+	         (test->settings->blocks != 0 && (test->blocks_sent >= test->settings->blocks ||
+						  test->blocks_received >= test->settings->blocks)))) {
 
 		// Unset non-blocking for non-UDP tests
 		if (test->protocol->id != Pudp) {
@@ -564,7 +573,7 @@ iperf_run_client(struct iperf_test * test)
 		cpu_util(test->cpu_util);
 		test->stats_callback(test);
 		if (iperf_set_send_state(test, TEST_END) != 0)
-		    return -1;
+                    goto cleanup_and_fail;
 	    }
 	}
 	// If we're in reverse mode, continue draining the data
@@ -574,7 +583,7 @@ iperf_run_client(struct iperf_test * test)
 	// from the client side.
 	else if (test->mode == RECEIVER && test->state == TEST_END) {
 	    if (iperf_recv(test, &read_set) < 0)
-		return -1;
+		goto cleanup_and_fail;
 	}
     }
 
@@ -589,4 +598,11 @@ iperf_run_client(struct iperf_test * test)
     iflush(test);
 
     return 0;
+
+  cleanup_and_fail:
+    iperf_client_end(test);
+    if (test->json_output)
+	iperf_json_finish(test);
+    iflush(test);
+    return -1;
 }
